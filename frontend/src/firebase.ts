0cb71820942043df0878ab5a087cb37aad78ddbe
Anna Kruger <krugerannav@gmail.com>
2025-11-04 23:14:11 -0600

    adjusting merge conflicts

--- frontend/src/firebase.ts
+++ frontend/src/firebase.ts
@@ -1,5 +1,6 @@
 // src/firebase.ts
 import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
 import { getAuth, GoogleAuthProvider } from "firebase/auth"; // ✅ added GoogleAuthProvider
 import { getFirestore } from "firebase/firestore";
 
@@ -15,5 +16,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+export const storage = getStorage(app);
 export const db = getFirestore(app);
 export const googleProvider = new GoogleAuthProvider(); // ✅ added this line
