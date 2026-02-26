7cd531ecc71d9e46f4dc4dc6853e34e4c68bc545
Austin Moser <austinmosercs@gmail.com>
2026-02-26 16:00:16 -0600

    refactor: remove obsolete test files and simplify Jest configuration

--- backend/index.test.js
+++
@@ -1,7 +0,0 @@
-const indexModule = require('./index');
-
-describe('index.js', () => {
-  it('should be importable', () => {
-    expect(indexModule).toBeDefined();
-  });
-});
--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,11 +1,10 @@
 module.exports = {
   testEnvironment: "node",
-  roots: ["<rootDir>", "<rootDir>/__tests__", "<rootDir>/scripts"],
-  testMatch: ["**/*.test.js", "**/*.test.mjs"],
+  roots: ["<rootDir>", "<rootDir>/__tests__"],
+  testMatch: ["**/*.test.js"],
   setupFiles: ["<rootDir>/jest.setup.js"],
   clearMocks: true,
   collectCoverageFrom: ["server.js", "helpers.js", "routes/fairs.js"],
   coverageDirectory: "coverage",
   coverageReporters: ["text", "lcov"],
-  transform: {}, // disables babel/jest transform for .mjs
 };
